23d3724d8b831bd37e8bb0e907d8663768d5828d
marquesleandro <marquesleandro67@gmail.com>
2018-12-21 15:42:55 -0200

    modification

--- bc_apply.py
+++ bc_apply.py
@@ -1,5 +1,5 @@
 # ==========================================
-# Code created by Leandro Marques at 12/2017
+# Code created by Leandro Marques at 12/2018
 # Gesar Search Group
 # State University of the Rio de Janeiro
 # e-mail: marquesleandro67@gmail.com
@@ -10,9 +10,16 @@
 
 # ------------------------------------------------------------------
 # Use:
-#bc_neumann_vx,bc_dirichlet_vx,LHS_vx,bc_1_vx,bc_2_vx,ibc_vx = 
-#b_bc(mesh.npoints, mesh.x, mesh.y, bc, mesh.neumann_edges[1], mesh.dirichlet_pts[1], LHS_vx0, mesh.neighbors_nodes)
 
+# Applying vx condition
+#condition_xvelocity = bc_apply.Linear(mesh.npoints,mesh.x,mesh.y,bc)
+#condition_xvelocity.neumann_condition(mesh.neumann_edges[1])
+#condition_xvelocity.dirichlet_condition(mesh.dirichlet_pts[1])
+#condition_xvelocity.gaussian_elimination(LHS_vx0,mesh.neighbors_nodes)
+
+# Applying psi condition
+#condition_psi = bc_apply2.Poiseuille(mesh.npoints,mesh.x,mesh.y,bc)
+#condition_psi.streamfunction_condition(mesh.dirichlet_pts[3],LHS_psi0,mesh.neighbors_nodes)
 # ------------------------------------------------------------------
 
 
@@ -21,141 +28,152 @@ import scipy.sparse as sps
 import scipy.sparse.linalg
 
 
-def b_bc(_npoints, _x, _y, _bc, _neumann_edges,_dirichlet_pts, _LHS0, _neighbors_nodes): #For Method 2 and 3
-#def b_bc(_npoints, _x, _y, _bc, _neumann_edges,_dirichlet_pts, _LHS0): #For Method 1
+class Poiseuille:
+ def __init__(_self, _npoints, _x, _y, _bc):
+  _self.npoints = _npoints
+  _self.x = _x
+  _self.y = _y
+  _self.bc = _bc
 
- bc_neumann = np.zeros([_npoints,1], dtype = float) 
- bc_dirichlet = np.zeros([_npoints,1], dtype = float) 
- ibc = [] 
- #bc_1 = np.zeros([1,_npoints], dtype = float) #For numpy array solve
- bc_1 = np.zeros([_npoints,1], dtype = float) #For scipy array solve
- bc_2 = np.ones([_npoints,1], dtype = float) 
- _LHS = sps.lil_matrix.copy(_LHS0)
+ 
+ def neumann_condition(_self, _neumann_edges):
+  _self.bc_neumann = np.zeros([_self.npoints,1], dtype = float) 
+  _self.neumann_edges = _neumann_edges 
+ 
+  for i in range(0, len(_self.neumann_edges)):
+   line = _self.neumann_edges[i][0] - 1
+   v1 = _self.neumann_edges[i][1] - 1
+   v2 = _self.neumann_edges[i][2] - 1
 
-
-
- # Neumann condition 
- for i in range(0, len(_neumann_edges)):
-  line = _neumann_edges[i][0] - 1
-  v1 = _neumann_edges[i][1] - 1
-  v2 = _neumann_edges[i][2] - 1
-
-  x = _x[v1] - _x[v2]
-  y = _y[v1] - _y[v2]
-  length = np.sqrt(x**2 + y**2)
+   x = _self.x[v1] - _self.x[v2]
+   y = _self.y[v1] - _self.y[v2]
+   length = np.sqrt(x**2 + y**2)
   
-  bc_neumann[v1] += (_bc[line]*length) / 2. 
-  bc_neumann[v2] += (_bc[line]*length) / 2. 
+   _self.bc_neumann[v1] += (_self.bc[line]*length) / 2. 
+   _self.bc_neumann[v2] += (_self.bc[line]*length) / 2. 
 
- # Dirichlet condition
- for i in range(0, len(_dirichlet_pts)):
-  line = _dirichlet_pts[i][0] - 1
-  v1 = _dirichlet_pts[i][1] - 1
-  v2 = _dirichlet_pts[i][2] - 1
 
-  bc_1[v1] = _bc[line]
-  bc_1[v2] = _bc[line]
+ def dirichlet_condition(_self, _dirichlet_pts):
+  _self.bc_dirichlet = np.zeros([_self.npoints,1], dtype = float) 
+  _self.ibc = [] 
+  #_self.bc_1 = np.zeros([1,_self.npoints], dtype = float) #For numpy array solve
+  _self.bc_1 = np.zeros([_self.npoints,1], dtype = float) #For scipy array solve
+  _self.bc_2 = np.ones([_self.npoints,1], dtype = float) 
+  _self.dirichlet_pts = _dirichlet_pts
+ 
 
-  bc_neumann[v1] = 0.0 #Dirichlet condition is preferential
-  bc_neumann[v2] = 0.0 #Dirichlet condition is preferential
+  for i in range(0, len(_self.dirichlet_pts)):
+   line = _self.dirichlet_pts[i][0] - 1
+   v1 = _self.dirichlet_pts[i][1] - 1
+   v2 = _self.dirichlet_pts[i][2] - 1
 
-  ibc.append(v1)
-  ibc.append(v2)
+   _self.bc_1[v1] = _self.bc[line]
+   _self.bc_1[v2] = _self.bc[line]
+
+   _self.bc_neumann[v1] = 0.0 #Dirichlet condition is preferential
+   _self.bc_neumann[v2] = 0.0 #Dirichlet condition is preferential
+
+   _self.ibc.append(v1)
+   _self.ibc.append(v2)
    
- ibc = np.unique(ibc)
+  _self.ibc = np.unique(_self.ibc)
 
- # Gaussian elimination
- # Scipy sparse - Method 1
-# _LHS = _LHS.tolil() #For scipy array
-# for i in range(0,len(ibc)):
-#  mm = ibc[i]
-#  #bc_1 -= _LHS[:,mm]*bc_dirichlet[mm] #For numpy array
-#  bc_1 -= _LHS[:,mm].todense()*bc_dirichlet[mm] #For scipy array
-#  _LHS[:,mm] = 0.0
-#  _LHS[mm,:] = 0.0
-#  _LHS[mm,mm] = 1.0
-#  #bc_1[0][mm] = bc_dirichlet[mm] #For numpy array solve
-#  bc_1[mm] = bc_dirichlet[mm] #For scipy array solve
-#  bc_2[mm] = 0.0
-# #bc_1 = np.transpose(bc_1) #For numpy array solve
 
-# # Scipy sparse - Method 2
-# for i in range(0,len(ibc)):
-#  mm = ibc[i]
+ def gaussian_elimination(_self, _LHS0, _neighbors_nodes):
+  _self.LHS = sps.lil_matrix.copy(_LHS0)
+  _self.neighbors_nodes = _neighbors_nodes
+
+#  # Scipy sparse - Method 1
+#  _self.LHS = _self.LHS.tolil() #For scipy array
+#  for i in range(0,len(_self.ibc)):
+#   mm = _self.ibc[i]
+#   #_self.bc_dirichlet -= _self.LHS[:,mm]*_self.bc_1[mm] #For numpy array
+#   _self.bc_dirichlet -= _self.LHS[:,mm].todense()*_self.bc_1[mm] #For scipy array
+#   _self.LHS[:,mm] = 0.0
+#   _self.LHS[mm,:] = 0.0
+#   _self.LHS[mm,mm] = 1.0
+#   #_self.bc_dirichlet[0][mm] = _self.bc_1[mm] #For numpy array solve
+#   _self.bc_dirichlet[mm] = _self.bc_1[mm] #For scipy array solve
+#   _self.bc_2[mm] = 0.0
+#  #_self.bc_1 = np.transpose(_self.bc_1) #For numpy array solve
+
+#  # Scipy sparse - Method 2
+#  for i in range(0,len(_self.ibc)):
+#   mm = _self.ibc[i]
 # 
-#  for nn in _neighbors_nodes[mm]:
-#   bc_1[nn] -= float(_LHS[nn,mm]*bc_dirichlet[mm])
-#   _LHS[nn,mm] = 0.0
-#   _LHS[mm,nn] = 0.0
+#   for nn in _self.neighbors_nodes[mm]:
+#    _self.bc_dirichlet[nn] -= float(_self.LHS[nn,mm]*_self.bc_1[mm])
+#    _self.LHS[nn,mm] = 0.0
+#    _self.LHS[mm,nn] = 0.0
 #   
-#  _LHS[mm,mm] = 1.0
-#  bc_1[mm] = bc_dirichlet[mm]
-#  bc_2[mm] = 0.0
+#   _self.LHS[mm,mm] = 1.0
+#   _self.bc_dirichlet[mm] = _self.bc_1[mm]
+#   _self.bc_2[mm] = 0.0
  
 # # Scipy sparse - Method 3
- for mm in ibc:
-  for nn in _neighbors_nodes[mm]:
-   bc_dirichlet[nn] -= float(_LHS[nn,mm]*bc_1[mm])
-   _LHS[nn,mm] = 0.0
-   _LHS[mm,nn] = 0.0
+  for mm in _self.ibc:
+   for nn in _self.neighbors_nodes[mm]:
+    _self.bc_dirichlet[nn] -= float(_self.LHS[nn,mm]*_self.bc_1[mm])
+    _self.LHS[nn,mm] = 0.0
+    _self.LHS[mm,nn] = 0.0
    
-  _LHS[mm,mm] = 1.0
-  bc_dirichlet[mm] = bc_1[mm]
-  bc_2[mm] = 0.0
-  
+   _self.LHS[mm,mm] = 1.0
+   _self.bc_dirichlet[mm] = _self.bc_1[mm]
+   _self.bc_2[mm] = 0.0
  
- return bc_neumann, bc_dirichlet, _LHS, bc_1, bc_2, ibc
 
 
+ def streamfunction_condition(_self, _dirichlet_pts, _LHS0, _neighbors_nodes):
+  _self.bc_dirichlet = np.zeros([_self.npoints,1], dtype = float) 
+  _self.ibc = [] 
+  _self.bc_1 = np.zeros([_self.npoints,1], dtype = float) #For scipy array solve
+  _self.bc_2 = np.ones([_self.npoints,1], dtype = float) 
+  _self.LHS = sps.lil_matrix.copy(_LHS0)
+  _self.dirichlet_pts = _dirichlet_pts
+  _self.neighbors_nodes = _neighbors_nodes
 
-def streamfunction_bc(_npoints, _x, _y, _bc, _neumann_edges,_dirichlet_pts, _LHS0, _neighbors_nodes):
- bc_neumann = np.zeros([_npoints,1], dtype = float) 
- bc_dirichlet = np.zeros([_npoints,1], dtype = float) 
- ibc = [] 
- bc_1 = np.zeros([_npoints,1], dtype = float) #For scipy array solve
- bc_2 = np.ones([_npoints,1], dtype = float) 
- _LHS = sps.lil_matrix.copy(_LHS0)
+  # Dirichlet condition
+  for i in range(0, len(_self.dirichlet_pts)):
+   line = _self.dirichlet_pts[i][0] - 1
+   v1 = _self.dirichlet_pts[i][1] - 1
+   v2 = _self.dirichlet_pts[i][2] - 1
 
- # Dirichlet condition
- for i in range(0, len(_dirichlet_pts)):
-  line = _dirichlet_pts[i][0] - 1
-  v1 = _dirichlet_pts[i][1] - 1
-  v2 = _dirichlet_pts[i][2] - 1
-
-  if line == 8:
-   bc_1[v1] = 0.0
-   bc_1[v2] = 0.0
+   if line == 8:
+    _self.bc_1[v1] = 0.0
+    _self.bc_1[v2] = 0.0
  
-   ibc.append(v1)
-   ibc.append(v2)
+    _self.ibc.append(v1)
+    _self.ibc.append(v2)
 
-  elif line == 11:
-   bc_1[v1] = 1.0
-   bc_1[v2] = 1.0
+   elif line == 11:
+    _self.bc_1[v1] = 1.0
+    _self.bc_1[v2] = 1.0
 
-   ibc.append(v1)
-   ibc.append(v2)
+    _self.ibc.append(v1)
+    _self.ibc.append(v2)
 
-  else:
-   bc_1[v1] = _y[v1]
-   bc_1[v2] = _y[v2]
+   else:
+    _self.bc_1[v1] = _self.y[v1]
+    _self.bc_1[v2] = _self.y[v2]
 
-   ibc.append(v1)
-   ibc.append(v2)
+    _self.ibc.append(v1)
+    _self.ibc.append(v2)
 
- ibc = np.unique(ibc)
+  _self.ibc = np.unique(_self.ibc)
 
 
- # Gaussian elimination for psi
- for mm in ibc:
-  for nn in _neighbors_nodes[mm]:
-   bc_dirichlet[nn] -= float(_LHS[nn,mm]*bc_1[mm])
-   _LHS[nn,mm] = 0.0
-   _LHS[mm,nn] = 0.0
+  # Gaussian elimination for psi
+  for mm in _self.ibc:
+   for nn in _self.neighbors_nodes[mm]:
+    _self.bc_dirichlet[nn] -= float(_self.LHS[nn,mm]*_self.bc_1[mm])
+    _self.LHS[nn,mm] = 0.0
+    _self.LHS[mm,nn] = 0.0
    
-  _LHS[mm,mm] = 1.0
-  bc_dirichlet[mm] = bc_1[mm]
-  bc_2[mm] = 0.0
+   _self.LHS[mm,mm] = 1.0
+   _self.bc_dirichlet[mm] = _self.bc_1[mm]
+   _self.bc_2[mm] = 0.0
  
- return bc_neumann, bc_dirichlet, _LHS, bc_1, bc_2, ibc
  
+
+
+
